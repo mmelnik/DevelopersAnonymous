633e3839860f1712e38731806d9c307d3b5a6e4e
Mihail Melnik <likethomasyorke@gmail.com>
2025-03-14 00:05:53 +0300

    refactoring: extract method GildedRose.increaseQuality() <eom>

--- kotlin/src/main/kotlin/org/joyofcoding/objectcalisthenics/GildedRose.kt
+++ kotlin/src/main/kotlin/org/joyofcoding/objectcalisthenics/GildedRose.kt
@@ -28,18 +28,18 @@ class GildedRose {
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    increaseQuality(item)
 
                     if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                increaseQuality(item)
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                increaseQuality(item)
                             }
                         }
                     }
@@ -63,11 +63,15 @@ class GildedRose {
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        increaseQuality(item)
                     }
                 }
             }
         }
+    }
+
+    private fun increaseQuality(item: Item) {
+        item.quality = item.quality + 1
     }
 
 }
